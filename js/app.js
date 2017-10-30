08fbcef5cf9560f73890b4d7307b0055bf820860
sushilcs111 <sushil.sud@gmail.com>
2017-10-30 18:47:23 +0530

    meh

--- js/app.js
+++ js/app.js
@@ -1,10 +1,8 @@
 var app = angular.module("occupyparking", ['ngSanitize']);
 
-
-
-
-
 app.controller("homeCtrl", function ($scope, $http) {
+
+    $scope.user = get('user')
 
     $scope.bookings = [];
     if (online()) {
@@ -108,9 +106,74 @@ app.controller("homeCtrl", function ($scope, $http) {
 
     }
 
+
+
+    $scope.addBooking = function () {
+        var data = $("#addBooking").serialize();
+        var url = apiEndpoint + 'addBooking';
+        var type = 'POST';
+
+
+        var r = confirm("Confirm Booking?");
+        if (r == true) {
+
+            if (online()) {
+                $.ajax({
+                    url: url,
+                    type: type,
+                    data: data,
+                    success: function (data) {
+                        if (data == 'Booking added') {
+                            alert("Booking Added.");
+                            $('#modal1').modal('close');
+                            $('#addBooking').each(function () {
+                                this.reset();
+                            });
+                        } else {
+                            alert(data);
+                        }
+                    }
+                });
+
+            } else {
+                if (get('toSync') == null || !isArray(get('toSync')))
+                    set("toSync", JSON.stringify([]));
+
+                var toSync = get('toSync');
+                toSync.push({url: url, data: data, type: type, pushedOn: new Date()});
+                set('toSync', toSync);
+
+            }
+        }
+    }
+
 });
 
 function req(url, data, type) {
 
 
 }
+
+
+$(document).ready(function () {
+    $("input[name='search']").keyup(function (e) {
+        var sterm = $(this).val();
+        if (sterm == "") {
+
+            $(".contact").show();
+            $(".ftest1").click();
+
+        } else {
+
+            $(".contact").hide();
+            $(".contact[data-booking-id='" + sterm + "']").show();
+            $(".f" + $(".contact[data-booking-id='" + sterm + "']").parent().parent().parent().parent().attr('id')).click()
+
+            $(".contact[data-vehicle-number='" + sterm + "']").show();
+            $(".f" + $(".contact[data-vehicle-number='" + sterm + "']").parent().parent().parent().parent().attr('id')).click()
+
+
+        }
+
+    });
+});
